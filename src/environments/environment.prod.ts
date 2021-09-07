5f116c17e82295142213b2c042817c5b75dccdd2
Pedro <pedroseneriz01@gmail.com>
2021-09-07 11:50:29 -0300

    aver

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBARaU4cewozOiRf251JctxNoTgrNuZ6Fg",
+    authDomain: "senerizsdj.firebaseapp.com",
+    projectId: "senerizsdj",
+    storageBucket: "senerizsdj.appspot.com",
+    messagingSenderId: "157333152837",
+    appId: "1:157333152837:web:3b82e89061430c9a95ef2c",
+    measurementId: "G-139VN0NPLH"
+  }
 };
